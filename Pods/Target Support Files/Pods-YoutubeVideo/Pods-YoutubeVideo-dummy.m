02fe0ddbc6b0ab4e0f8fe8a950b5b7c0c33f186e
msnbek <mahmutsenbek@icloud.com>
2023-01-24 12:51:27 +0300

    youtube video playing

---
+++ Pods/Target Support Files/Pods-YoutubeVideo/Pods-YoutubeVideo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_YoutubeVideo : NSObject
+@end
+@implementation PodsDummy_Pods_YoutubeVideo
+@end
--- YoutubeVideo/ViewController.swift
+++ YoutubeVideo/ViewController.swift
@@ -6,14 +6,57 @@
 //
 
 import UIKit
+import YoutubePlayer_in_WKWebView
+import AVKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKYTPlayerViewDelegate {
+    var youtubeId = ""
+    
+    @IBOutlet weak var player: WKYTPlayerView!
+    var youtubeUrl = "https://www.youtube.com/watch?v=WnSSvz9ry_w&ab_channel=Webkul"
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.youtubeId = youtubeUrl.youtubeID ?? ""
+         print("YoutubeId is: \(self.youtubeId)")
+     loadvideo()
+            self.player.delegate = self
+       
     }
+    
+    func loadvideo() {
+        DispatchQueue.main.async {
+            self.player.load(withVideoId: self.youtubeId, playerVars: ["playsinline":"0"])
+        }
+       
+    }
+    
+    func playerViewDidBecomeReady(_ playerView: WKYTPlayerView) {
+        DispatchQueue.main.async {
+            self.player.playVideo()
+        }
+    
+     }
+     override func viewDidDisappear(_ animated: Bool) {
+     player.stopVideo()
+     
+     }
 
 
+}
+
+extension String {
+    var youtubeID: String? {
+        let pattern = "((?<=(v|V)/)|(?<=be/)|(?<=(\\?|\\&)v=)|(?<=embed/))([\\w-]++)"
+        
+        let regex = try? NSRegularExpression(pattern: pattern, options: .caseInsensitive)
+        let range = NSRange(location: 0, length: count)
+        
+        guard let result = regex?.firstMatch(in: self, range: range) else {
+            return nil
+        }
+        
+        return (self as NSString).substring(with: result.range)
+    }
+    
 }
 
